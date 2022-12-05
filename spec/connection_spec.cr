0645b212ba40b157ebd0a524dfa5a1287f7f4ec2
silkPK <alessandro.rusani@gmail.com>
2022-12-05 09:50:29 +0100

    spec

---
+++ spec/connection_spec.cr
@@ -0,0 +1,15 @@
+require "./spec_helper"
+
+describe Connection do
+  it "opens a connection without the pool" do
+    with_cnn do |cnn|
+      cnn.should be_a(ODPI::Connection)
+
+      #cnn.exec "BEGIN EXECUTE IMMEDIATE 'drop table person'; EXCEPTION WHEN OTHERS THEN NULL; END;"
+      #cnn.exec "create table person (name varchar(10), age number)"
+      cnn.exec "insert into person (name,age) values ('Ben Buddy', 28)"
+
+      #cnn.scalar("select count(*) from person").should eq(1)
+    end
+  end
+end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,16 @@
 require "spec"
-require "../src/crystal-odpi"
+require "../src/odpi"
+
+include ODPI
+
+DB_URL = ENV["DATABASE_URL"]? || "odpi:///"
+
+def with_db(&block : DB::Database ->)
+  DB.open "#{DB_URL}", &block
+end
+
+def with_cnn(&block : DB::Connection ->)
+    puts DB_URL
+  DB.connect "#{DB_URL}", &block
+end
+
